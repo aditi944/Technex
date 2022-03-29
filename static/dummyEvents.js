febd8712b17c60dde293ce27a8507a15084ed64e
mohammad shadab azam ansari <34206533+tipuboyy123@users.noreply.github.com>
2022-03-29 13:57:46 +0530

    Add files via upload

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 17,
+    from: '0xQazi',
+    to: '0xDavid',
+    date: '1 day ago',
+  },
+  {
+    price: 17,
+    from: '0xQazi',
+    to: '0xDavid',
+    date: '1 day ago',
+  },
+  {
+    price: 17,
+    from: '0xQazi',
+    to: '0xDavid',
+    date: '1 day ago',
+  },
+  {
+    price: 17,
+    from: '0xQazi',
+    to: '0xDavid',
+    date: '1 day ago',
+  },
+]
